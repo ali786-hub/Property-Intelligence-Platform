255de7fac1c9ecc74f01d1a9597fcce8f25b21de
ali786-hub <kofking166@gmail.com>
2026-07-28 01:32:48 +0500

    fixed threading issue in gold

--- src/loading/gold_publish.py
+++ src/loading/gold_publish.py
@@ -227,6 +227,8 @@ def process_table_bulk(catalog, file_list, purpose_filter, iceberg_table_name, a
     con = duckdb.connect()
     con.execute("PRAGMA memory_limit='4GB'")
     con.execute("PRAGMA temp_directory='/tmp/duckdb_temp'")
+    con.execute("PRAGMA threads=2")
+    con.execute("PRAGMA preserve_insertion_order=false")
 
     if SILVER_ZONE and (SILVER_ZONE.startswith("abfs://") or SILVER_ZONE.startswith("azure://")):
         con.register_filesystem(silver_fs)
